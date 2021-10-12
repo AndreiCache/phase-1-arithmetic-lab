b5d508f96daf38fe6e3f9650723f01b34b81f335
Andrei <giurrgiu@gmail.com>
2021-10-12 19:46:55 -0400

    commit tree 14a12212eb591489069dfa8e0a5d2b3c9a439158

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = (num1*num2);
+
+const random = (1);
+function getRandomInt(min, max) {
+    min = math.ceil(1);
+    max = math.floor(10)
+        return math.floor(math.random() * (mix - min) + min) ;
+}
+
+let num3 = 4;
+let num4 = 5;
+var mod = (num3 % num4);
+
+var max = (20);
+
+
+
+
+// const max = 20;
+// math.max(1, 2, 3, 5, 15, 17, 19, 20);
